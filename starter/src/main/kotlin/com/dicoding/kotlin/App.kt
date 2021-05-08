fc1786e686537c842455531de24e52f3f40212e8
Reski Mulud Muchamad <reski.mulud@gmail.com>
2021-05-08 19:28:19 +0700

    update App.kt

--- starter/src/main/kotlin/com/dicoding/kotlin/App.kt
+++ starter/src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -1,5 +1,19 @@
 package com.dicoding.kotlin
 
+// main function
 fun main() {
-    println("Hello world")
+    println("Hello Kotlin!")
+
+
+    // single line comment
+    /*
+    multi line comments
+    Hello Kotlin
+     */
+
+    val name = "Reski Mulud"
+    print("Hello, my name is ")
+    println(name)
+    
+    print(if (true) "Always True" else "Always False")
 }
