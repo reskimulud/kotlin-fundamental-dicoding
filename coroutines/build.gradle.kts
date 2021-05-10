e53bfd43152184a9cc6bf723d3914bb7190d5f19
Reski Mulud Muchamad <reski.mulud@gmail.com>
2021-05-11 00:24:18 +0700

    belajar coroutines yang ada di kotlin

---
+++ coroutines/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    kotlin("jvm") version "1.5.0"
+}
+
+group = "com.dicoding.kotlin.coroutines"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ coroutines/src/main/kotlin/com/dicoding/kotlin/coroutines/Caroutines.kt
@@ -0,0 +1,12 @@
+package com.dicoding.kotlin.coroutines
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking{
+    launch {
+        delay(1000L)
+        println("Coroutines!")
+    }
+    println("Hello,")
+    delay(2000L)
+}
